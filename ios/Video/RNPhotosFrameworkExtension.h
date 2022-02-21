731cdb734a932f8c35d94eaceae463c1cf7d0fd1
Cristi Conecini <cristi.conecini@gmail.com>
2022-02-21 23:32:02 +0100

    ph: uri scheme support

---
+++ ios/Video/RNPhotosFrameworkExtension.h
@@ -0,0 +1,13 @@
+//
+//  RNPhotosFrameworkExtension.h
+//  RCTVideo
+//
+//  Created by Cristi Conecini on 21.02.22.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#ifndef RNPhotosFrameworkExtension_h
+#define RNPhotosFrameworkExtension_h
+
+
+#endif /* RNPhotosFrameworkExtension_h */
---
+++ ios/Video/RNPhotosFrameworkExtension.m
@@ -0,0 +1,9 @@
+//
+//  RNPhotosFrameworkExtension.m
+//  RCTVideo
+//
+//  Created by Cristi Conecini on 21.02.22.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
